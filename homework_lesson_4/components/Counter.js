0c4dfc2c9eab1672de57a9246a5ac1837fe0bfd6
Andrej Dergavko <andreidergavko@gmail.com>
2020-01-24 16:41:49 +0300

    homework lesson 4

--- homework_lesson_4/components/Counter.js
+++ homework_lesson_4/components/Counter.js
@@ -1,54 +1,56 @@
-
-
 class Counter {
-  constructor(props) {
+  constructor(props = {}) {
     this.startCount = props.startCount === undefined ? 0 : props.startCount;
     this.stepUp = props.stepUp === undefined ? 1 : props.stepUp;
     this.stepDown = props.stepDown === undefined ? 1 : props.stepDown;
     this.limitUp = props.limitUp === undefined ? Infinity : props.limitUp;
-    this.limitDown =
-      props.limitDown === undefined ? -Infinity : props.limitDown;
+    this.limitDown = props.limitDown === undefined ? -Infinity : props.limitDown;
 
     this.count = this.startCount;
   }
 
-  increaseCounter() {
+  increaseCounter(event) {
     const newCount = this.count + this.stepUp;
+    const countDisplay = event.target.parentElement.querySelector(".count__count-display");
 
     if (newCount <= this.limitUp) {
       this.count = newCount;
-      this.refreshCount();
+      this.refreshCount(countDisplay);
     }
   }
 
-  decreaseCounter() {
+  decreaseCounter(event) {
     const newCount = this.count - this.stepDown;
+    const countDisplay = event.target.parentElement.querySelector(".count__count-display");
 
     if (newCount >= this.limitDown) {
       this.count = newCount;
-      this.refreshCount();
+      this.refreshCount(countDisplay);
     }
   }
 
-  refreshCount() {
-    document.getElementById("countBox").innerHTML = this.count;
+  refreshCount(elem) {
+    elem.innerHTML = this.count;
   }
 
   render() {
     const buttonUp = document.createElement("button");
+    buttonUp.className = "count__button count__button_up";
     buttonUp.append("+");
     buttonUp.addEventListener("click", this.increaseCounter.bind(this));
 
     const buttonDown = document.createElement("button");
+    buttonDown.className = "count__button count__button_down";
     buttonDown.append("-");
     buttonDown.addEventListener("click", this.decreaseCounter.bind(this));
 
-    const countBox = document.createElement("div");
-    countBox.id = "countBox";
-    countBox.append(this.count);
+    const countDisplay = document.createElement("div");
+    countDisplay.className = "count__count-display";
+    countDisplay.append(this.count);
 
     const wrapper = document.createElement("div");
-    wrapper.append(...[buttonUp, countBox, buttonDown]);
+    wrapper.className = "count";
+    wrapper.append(buttonUp, countDisplay, buttonDown);
 
     return wrapper;
   }
--- homework_lesson_4/index.js
+++ homework_lesson_4/index.js
@@ -1,14 +1,20 @@
 import { Counter } from "./components/Counter.js";
-import './components/Counter.css'
 
-const o = new Counter({
+const count1 = new Counter({
   startCount: 10,
   stepUp: 2,
   stepDown: 10,
   limitUp: 20,
   limitDown: -20
 });
-console.log(o.render());
 
-document.getElementById('root').append(o.render())
+const count2 = new Counter({
+  startCount: 10,
+  stepDown: 10,
+  limitDown: -20
+});
+
+const count3 = new Counter();
+
+document.getElementById("root").append(count1.render(), count2.render(), count3.render());
 
